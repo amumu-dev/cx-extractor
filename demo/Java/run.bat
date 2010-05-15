d880c8ce7d0187227f8d22fcd78a4789aae2ed8e
cx3180 <cx3180@d509e544-38d3-8e0f-39ad-7e6ff181509b>
2010-05-15 09:45:44 +0000

    add demos.
    git-svn-id: http://cx-extractor.googlecode.com/svn/trunk@7 d509e544-38d3-8e0f-39ad-7e6ff181509b

---
+++ demo/Java/run.bat
@@ -0,0 +1,13 @@
+javac *.java
+
+java UseDemo http://ent.qq.com/a/20100417/000119.htm
+
+java UseDemo http://ent.sina.com.cn/y/2010-04-18/08332932833.shtml
+		
+java UseDemo http://ent.qq.com/a/20100416/000208.htm
+		
+java UseDemo http://ent.sina.com.cn/y/2010-04-18/15432932937.shtml
+		
+java UseDemo http://ent.qq.com/a/20100417/000119.htm
+		
+java UseDemo http://news.hit.edu.cn/articles/2010/04-12/04093006.htm
