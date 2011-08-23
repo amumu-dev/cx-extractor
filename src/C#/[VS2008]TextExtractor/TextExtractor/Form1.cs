c680336276429d199ea4ab23f6b1afe82182de18
zfannn@gmail.com <zfannn@gmail.com@d509e544-38d3-8e0f-39ad-7e6ff181509b>
2011-08-23 02:51:26 +0000

    1.新增C#版本的DEMO 2.提取正文时可以选择拼接零散内容或不拼接
    git-svn-id: http://cx-extractor.googlecode.com/svn/trunk@39 d509e544-38d3-8e0f-39ad-7e6ff181509b

---
+++ src/C#/[VS2008]TextExtractor/TextExtractor/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace TextExtractor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonExtract_Click(object sender, EventArgs e)
+        {
+            Encoding GB18030  = Encoding.GetEncoding("GB18030");   // GB18030兼容GBK和GB2312
+            Encoding UTF8     = Encoding.UTF8;
+            Encoding en       = UTF8;
+            bool bJoinMethond = true;
+
+            string url = Regex.Replace(textBoxURL.Text, @"(?is)\s*", "");
+            if (radioButtonGB18030.Checked)
+                en = GB18030;
+            if (radioButtonOnce.Checked)
+                bJoinMethond = false;
+
+            if (0 == url.Length)
+                MessageBox.Show("请输入网址", "提示");
+            else
+            {
+                string html = SourceHTML.GetUrlHTML(url, en);
+                if (html.Length != 0)
+                {
+                    TextExtract te = new TextExtract(html, bJoinMethond);
+
+                    textBoxTitle.Text              = te.title;
+                    richTextBoxText.Text           = te.content;
+                    webBrowserPreview.DocumentText = te.webPreview;
+                }
+            }
+
+        }
+
+    }
+}
---
+++ src/C#/[VS2008]TextExtractor/TextExtractor/sourceHTML.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace TextExtractor
+{
+
+    public static class SourceHTML
+    {
+        private static Encoding GB18030 = Encoding.GetEncoding("GB18030");   // GB18030兼容GBK和GB2312
+        private static Encoding UTF8    = Encoding.UTF8;
+
+        public static string GetUrlHTML(string url, Encoding en)
+        {
+            //数据包头部
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method    = "GET";
+            request.Accept    = "*/*";
+            request.UserAgent = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)";
+
+            //服务器返回的内容
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            if (response != null && HttpStatusCode.OK == response.StatusCode)
+            {
+                StreamReader sr = new StreamReader(response.GetResponseStream(), en);
+                return sr.ReadToEnd();
+            }
+
+            return "";
+        }
+
+    }
+
+}
